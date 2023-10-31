444df2dd3399e1b59f59443716d85287ad1e89ec
Adrien Sardi <98386232+Aupeka@users.noreply.github.com>
2023-10-31 14:04:47 +0100

    Update fonction.cpp
    norm_2 et err_l2 ajouté. Fonctionne en séquentiel !

--- Problème stationnaire, grille non strcturée/fonction.cpp
+++ Problème stationnaire, grille non strcturée/fonction.cpp
@@ -1,1 +1,20 @@
+#include "headers.hpp"
 
+double norm_2(ScaVector& u) { //size peut être enlevé
+    double size = u.size();
+    double accum = 0.;
+    for (int i = 0; i < size; ++i) {
+        accum += u(i)*u(i);
+    }
+    //MPI.Reduc
+    return sqrt(accum/size);
+}
+
+double erreur_l2(SpMatrix& M, ScaVector& v){
+    double size = M.rows();
+    
+    //Calcul de l'erreur
+    ScaVector err = v.transpose()*M*v;
+    
+    return sqrt(err(0)/size);
+}

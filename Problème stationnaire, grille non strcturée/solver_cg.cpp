8a429d33670af57a679c149367677067b0315ca0
Adrien Sardi <98386232+Aupeka@users.noreply.github.com>
2023-11-02 16:25:49 +0100

    Update and rename solver_cgm.cpp to solver_cg.cpp

---
+++ Problème stationnaire, grille non strcturée/solver_cg.cpp
@@ -0,0 +1,15 @@
+#include "headers.hpp"
+
+extern int myRank;
+extern int nbTasks;
+
+//================================================================================
+// Solution of the system Au=b with gradient conjugate
+//================================================================================
+
+void gradient(SpMatrix& A, ScaVector& b, ScaVector& u, Mesh& mesh, double tol, int maxit)
+{
+  if(myRank == 0)
+    cout << "== gradient conjugate" << endl;
+
+}

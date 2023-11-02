97870173f64dd8349c27da57cf62068ce719d0db
Adrien Sardi <98386232+Aupeka@users.noreply.github.com>
2023-11-02 17:12:35 +0100

    Update solver_cg.cpp

--- Problème stationnaire, grille non strcturée/solver_cg.cpp
+++ Problème stationnaire, grille non strcturée/solver_cg.cpp
@@ -12,4 +12,26 @@ void gradient_conjugate(SpMatrix& A, ScaVector& b, ScaVector& u, Mesh& mesh, dou
   if(myRank == 0)
     cout << "== gradient conjugate" << endl;
 
+  // Gradient Conjugate solver
+  double p = 1e2;
+  double alpha_;
+  int it = 0;
+
+  //Residu initialization
+  double p_0 = calcul_residu(A,b,u, mesh);
+
+  while (residuNorm/residuNorm_0 > tol && it < maxit){
+    // Update field
+    alpha_ = (p.transpose()*p)/(p.transpose()*A*p);
+    u += alpha_*p;
+    p = calcul_residu(A,b,u, mesh);
+
+    it++;
+  }
+
+if(myRank == 0){
+    cout << "   -> final iteration: " << it << " (prescribed max: " << maxit << ")" << endl;
+    cout << "   -> final residual: " << residuNorm/residuNorm_0 << " (prescribed tol: " << tol << ")" << endl;
+  }
+
 }

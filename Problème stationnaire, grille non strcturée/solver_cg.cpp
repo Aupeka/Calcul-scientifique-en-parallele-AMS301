563475000bc1385f256b5a6548db49fc4f369c55
Adrien Sardi <98386232+Aupeka@users.noreply.github.com>
2023-11-06 00:39:46 +0100

    Update solver_cg.cpp

--- Problème stationnaire, grille non strcturée/solver_cg.cpp
+++ Problème stationnaire, grille non strcturée/solver_cg.cpp
@@ -12,26 +12,51 @@ void gradient_conjugate(SpMatrix& A, ScaVector& b, ScaVector& u, Mesh& mesh, dou
   if(myRank == 0)
     cout << "== gradient conjugate" << endl;
 
-  // Gradient Conjugate solver
-  double p = 1e2;
-  double alpha_;
+  // Compute the solver parameters
+  int size = A.rows();
+  ScaVector p(size);
+  ScaVector alpha_(size);
+  
+  //Gradient Conjugate solver
   int it = 0;
+  p = b - A*u;
+  exchangeAddInterfMPI(p, mesh);
 
   //Residu initialization
-  double p_0 = calcul_residu(A,b,u, mesh);
+  double norm_p_0 = calcul_norm_residu(A,b,u, mesh);
+  double norm_p = 1e2;
+  double buff;
 
-  while (residuNorm/residuNorm_0 > tol && it < maxit){
+  while (norm_p/norm_p_0 > tol && it < maxit){
+    
+    // Compute alpha_
+    ScaVector Ap = A*p;
+    exchangeAddInterfMPI(Ap, mesh);
+
+    for (int i = 0; i < size; i++){
+        alpha_(i) = p(i)*p(i)/(p(i)*Ap(i));
+    }
+
     // Update field
-    alpha_ = (p.transpose()*p)/(p.transpose()*A*p);
-    u += alpha_*p;
-    p = calcul_residu(A,b,u, mesh);
+    for(int i=0; i<size; i++){
+      u(i) = u(i) + alpha_(i)*p(i); // /!\ Pbm ici, des NAN sortent mais je ne sais pas pourquoi ...
+    }
+    cout << "norm_test = " << u << endl;
+
+    //Update residu
+    update_residu(p,A,b,u, mesh);
+
+    //Update of norm_p
+    norm_p = norm_2(p);
+    buff = norm_p;
+    MPI_Allreduce (&buff, &norm_p, 1, MPI_DOUBLE , MPI_SUM, MPI_COMM_WORLD);
 
     it++;
   }
-
+  
 if(myRank == 0){
     cout << "   -> final iteration: " << it << " (prescribed max: " << maxit << ")" << endl;
-    cout << "   -> final residual: " << residuNorm/residuNorm_0 << " (prescribed tol: " << tol << ")" << endl;
+    cout << "   -> final residual: " << norm_p/norm_p_0 << " (prescribed tol: " << tol << ")" << endl;
   }
 
 }

aebcbd1fb9e3d67684e2620de11a2a2789b11f33
Adrien Sardi <98386232+Aupeka@users.noreply.github.com>
2023-11-14 14:32:52 +0100

    Update fonction.cpp

--- Problème stationnaire, grille non strcturée/fonction.cpp
+++ Problème stationnaire, grille non strcturée/fonction.cpp
@@ -14,24 +14,14 @@ double norm_2(ScaVector& u) {
 
 double norm_2_glo(ScaVector& u, Mesh& mesh) {
     removeInterfMPI(u,mesh);
-    double n_loc = (u.transpose()*u)(0);
+    double n_loc = (u.transpose()*u)(0)/u.size();
     double n_glo;
     
     MPI_Allreduce (&n_loc, &n_glo, 1, MPI_DOUBLE , MPI_SUM, MPI_COMM_WORLD);
 
-    return sqrt(n_glo); //size ?
+    return sqrt(n_glo); //size --> Plus haut
 }
 
-double produit_scalaire_glo(ScaVector& u, ScaVector& v, Mesh& mesh){
-    removeInterfMPI(u,mesh);
-    removeInterfMPI(v,mesh);
-    double ps_loc = (u.transpose()*v)(0);
-    double ps_glo;
-
-    MPI_Allreduce (&ps_loc, &ps_glo, 1, MPI_DOUBLE , MPI_SUM, MPI_COMM_WORLD);
-
-    return ps_glo; // Diviser par le size ????
-}
 
 double produit_scalaire(ScaVector& u, ScaVector& v){
     double accum = 0.;
@@ -41,22 +31,41 @@ double produit_scalaire(ScaVector& u, ScaVector& v){
     return accum;
 }
 
+double produit_scalaire_glo(ScaVector& u, ScaVector& v, Mesh& mesh){
+    removeInterfMPI(u,mesh);
+    removeInterfMPI(v,mesh);
+    double ps_loc = (u.transpose()*v)(0)/u.size();
+    double ps_glo;
+
+    MPI_Allreduce (&ps_loc, &ps_glo, 1, MPI_DOUBLE , MPI_SUM, MPI_COMM_WORLD);
+
+    return ps_glo; // Diviser par le size ????
+}
+
+
 double calcul_norm_residu(SpMatrix& A, ScaVector& b, ScaVector& u, Mesh& mesh){
     
     //Calcul du résidu
     ScaVector residu = b - A*u;
     exchangeAddInterfMPI(residu, mesh);
+    
+    double n_residu = norm_2_glo(residu, mesh);
+
+    /*
+    //Calcul résidu
     removeInterfMPI(residu,mesh);
     double n_residu = norm_2(residu);
-    
-    //MPI exchange
-    double buff = n_residu;
+
+        //MPI exchange
+    double buff = n_residu*n_residu; //Problème de Somme des racines
     MPI_Allreduce (&buff, &n_residu, 1, MPI_DOUBLE , MPI_SUM, MPI_COMM_WORLD);
+    */
 
     //return
     return n_residu;
 }
 
+/*
 void update_residu(ScaVector& residu, SpMatrix& A, ScaVector& b, ScaVector& u, Mesh& mesh){
     
     //Mise à jour du résidu
@@ -64,6 +73,7 @@ void update_residu(ScaVector& residu, SpMatrix& A, ScaVector& b, ScaVector& u, M
     exchangeAddInterfMPI(residu, mesh);
     removeInterfMPI(residu,mesh);
 }
+*/
 
 double erreur_l2(SpMatrix& M, ScaVector& v, Mesh& mesh){
     //Error
@@ -78,3 +88,5 @@ double erreur_l2(SpMatrix& M, ScaVector& v, Mesh& mesh){
     
     return n_err;
 }
+
+
